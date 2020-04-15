390854012c121f8a48c8bb5e9f38c4d0509321b6
Rishabh Singh <rishabhksingh@gmail.com>
2020-04-15 16:08:19 +0530

    Update opendistro-windows-build.sh

--- elasticsearch/linux_distributions/opendistro-windows-build.sh
+++ elasticsearch/linux_distributions/opendistro-windows-build.sh
@@ -29,7 +29,20 @@ mv $ROOT/elasticsearch-$ES_VERSION $ROOT/$PACKAGE-$OD_VERSION
 cd $ROOT
 #Making zip
 zip -r odfe-$OD_VERSION.zip $PACKAGE-$OD_VERSION
-ls -l
-pwd
+
+##Build Exe
+wget https://download-gcdn.ej-technologies.com/install4j/install4j_unix_8_0_4.tar.gz -P $ROOT
+tar -xzf $ROOT/install4j_unix_8_0_4.tar.gz --directory $ROOT
+rm -rf $ROOT/*tar*
+aws s3 cp s3://odfe-windows/ODFE.install4j $ROOT
+if [ "$?" -eq "1" ]
+then
+  echo "Install4j not available"
+  exit 1
+fi
+ls -ltr
+
+#Build the exe
+#$ROOT/install4j*/bin/install4jc -d $ROOT/EXE -D sourcedir=$ROOT/$PACKAGE-$OD_VERSION,version=$OD_VERSION --license=L-M8-AMAZON_DEVELOPMENT_CENTER_INDIA_PVT_LTD#50047687020001-3rhvir3mkx479#484b6 $ROOT/ODFE.install4j
 
 
